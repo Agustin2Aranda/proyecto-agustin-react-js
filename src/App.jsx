cfce5b309253612072da1176a46ca827f78c25c3
agustin aranda <agustindegiuseppearanda@gmail.com>
2025-09-25 17:11:08 -0300

    Se actualizo el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Body from './components/Body.jsx';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home.jsx';
 import Layout from './components/Layout.jsx';
 import Nosotros from './components/Nosotros.jsx';
 import Productos from './components/Productos.jsx';
@@ -20,26 +21,17 @@ function App() {
       <Header />
       <Body />
       <Footer />
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route index element={<Home/>}/>
+            <Route path="/nosotros" element={<Nosotros/>}/>
+            <Route path="/productos" element={<Productos/>}/>
+            <Route path="/*" element={<Error/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
